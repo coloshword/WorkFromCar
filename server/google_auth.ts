c241e71abb91cd2cff64d595d6b949a24535aa4e
Acero Liang <acero.liangli@stonybrook.edu>
2025-12-02 23:32:55 -0500

    setting up google auth:

---
+++ server/google_auth.ts
@@ -0,0 +1,9 @@
+import { google } from 'googleapis';
+
+// const oauth2Client = new google.auth.OAuth2(
+
+// )
+export function auth() {
+    console.log(`${process.env.GOOGLE_AUTH_CLIENT_ID}`)
+}
+
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,19 @@
 import koa from "koa";
 import http from 'http';
 import Router from '@koa/router';
+import { auth } from 'google_auth.ts';
 
 const server = new koa();
 const router = new Router();
 
-router.get('/hello_world', async (ctx) => {
+router.get('/test_endpoint', async (ctx) => {
   ctx.status = 200;
   ctx.body = "Hello world";
+});
+
+router.get('/login_with_google', async(ctx) => {
+  ctx.status = 200;
+  ctx.redirect("https://google.com");
 });
 
 server.use(router.routes());
@@ -16,6 +22,6 @@ server.use(router.routes());
   const httpServer = http.createServer(server.callback());
   const port = 3000; //TODO: make env var 
   httpServer.listen(port, () => {
-    console.log(`Server is running ✨`);
-  })
+    console.log(`Server is running ✨ ${port}`);
+  });
 })();
